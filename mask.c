dc48fca89fb8fffe83279acf46f6b21b11e6ace1
Aurlin Dhillon <aurlin@Aurlins-MacBook.local>
2018-11-09 11:56:49 -0800

    updoot4.0

--- mask.c
+++ mask.c
@@ -297,9 +297,8 @@ static inline long mask2(long oldImage[N][N], long newImage[N][N], int rows, int
       int c1 = j;
       if ((r1 != cols) || (c1 != rows)){
       newImage[i][j] += oldImage[c1][r1];
-      weight[i][j]++;
-
     }
+    weight[i][j]++;
   }
 }
   
